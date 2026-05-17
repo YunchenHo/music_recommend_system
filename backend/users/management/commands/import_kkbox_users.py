8cfaed3d74eccdd65e007b8b703aaf1f221d9d3a
董慧姸 <dhy.mg12@nycu.edu.tw>
2026-05-17 22:01:25 +0800

    改

--- backend/users/management/commands/import_kkbox_users.py
+++ backend/users/management/commands/import_kkbox_users.py
@@ -31,7 +31,7 @@ KKBOX_USERS = {
         "msno_id": 7203,
         "msno": "VHpRaOzuJSgJb5VfbMLKnB6wVuCfBR4YRiStxK7XrN4=",
     },
-    "english@gmail.com": {
+    "doonghuiyen@gmail.com": {
         "msno_id": 3456,
         "msno": "DqwB7smOAIbNnnQbWOpfsmy9znTwfDEQCW1I6ujFG48=",
     },
